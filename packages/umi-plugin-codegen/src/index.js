30b6286a18ab9794d69b911ebb9d634e8e87802d
DaoYuan <txp1035@gmail.com>
2019-06-30 16:06:51 +0800

    codegen

--- packages/umi-plugin-codegen/src/index.js
+++ packages/umi-plugin-codegen/src/index.js
@@ -1,25 +1,6 @@
-import { getData, convertData } from './utils/handleData';
-import { checkDir, generateFiles } from './utils/handleFile';
-
+import milady from 'milady';
 export default function(api, opts = {}) {
-  const { paths } = api;
-  api.registerCommand('txp', {}, args => {
-    getData(opts.url).then(json => {
-      let data;
-      try {
-        data = convertData(json);
-      } catch (error) {
-        console.log('转换数据出错/n', error);
-      }
-      checkDir(paths.cwd);
-      generateFiles(data, paths.cwd);
-    });
-    // handelData();
-    // writeData();
-    // console.log(args);
-
-    // console.log('txp');
-    // console.log(paths.absSrcPath);
-    // console.log(paths.cwd);
+  api.registerCommand('codegen', {}, args => {
+    milady(opts);
   });
 }
