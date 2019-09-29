72e8ef606f41d5e15ea53d1932e4a0874375ea22
DaoYuan <txp1035@gmail.com>
2019-09-29 18:46:10 +0800

    add check

--- packages/fine-dog/src/index.ts
+++ packages/fine-dog/src/index.ts
@@ -1,6 +1,23 @@
 export default function main() {
+  if (checkCode) {
+    return;
+  }
   const code = getCode();
   appendScript(code);
+}
+function checkCode() {
+  const head = document.querySelector('head');
+  if (head && !head.firstElementChild) {
+    return false;
+  }
+  if (
+    head &&
+    head.firstElementChild &&
+    head.firstElementChild.innerHTML === 'var _hmt = _hmt || []'
+  ) {
+    return true;
+  }
+  return false;
 }
 function appendScript(code: string) {
   // 定义元素
@@ -24,6 +41,8 @@ function getCode() {
   const code = {
     PosTest: 'c19649af12cb73de53b0f4ba717ae927',
     Pos: 'd94cd395390c9b72ed33667c613aad4e',
+    ZsyTest: 'd7452eccdfbcd8fc752084d2d206fb89',
+    Zsy: 'c35624cd71a9ba29d625ac2c07fcc609',
   };
   const { host } = window.location;
   if (host === 'partner.depotnextdoor.com') {
@@ -31,6 +50,12 @@ function getCode() {
   }
   if (host === 'partner-test.depotnextdoor.com') {
     return code.PosTest;
+  }
+  if (host === 'scusmile-manage.depotnextdoor.com') {
+    return code.Zsy;
+  }
+  if (host === 'scusmile-manage-test.depotnextdoor.com') {
+    return code.ZsyTest;
   }
   if (host === 'localhost:8000') {
     return code.PosTest;
