44cd6d622763993e42a44cf08f0574379df8fdd5
DaoYuan <txp1035@gmail.com>
2020-05-30 15:57:35 +0800

    feat: 配置化

--- packages/fine-dog/src/index.ts
+++ packages/fine-dog/src/index.ts
@@ -7,7 +7,8 @@ export default function main() {
   if (checkCode()) {
     return;
   }
-  const code = getCode();
+  const { host } = window.location;
+  const code = getCode(host);
   appendScript(code);
   logger();
 }
@@ -48,28 +49,8 @@ function appendScript(code: string) {
     head.insertBefore(hmtScript, head && head.firstElementChild);
   }
 }
-function getCode() {
-  const code = {
-    PosTest: 'c19649af12cb73de53b0f4ba717ae927',
-    Pos: 'd94cd395390c9b72ed33667c613aad4e',
-    ZsyTest: 'd7452eccdfbcd8fc752084d2d206fb89',
-    Zsy: 'c35624cd71a9ba29d625ac2c07fcc609',
-  };
-  const { host } = window.location;
-  if (host === 'partner.depotnextdoor.com') {
-    return code.Pos;
-  }
-  if (host === 'partner-test.depotnextdoor.com') {
-    return code.PosTest;
-  }
-  if (host === 'scusmile-manage.depotnextdoor.com') {
-    return code.Zsy;
-  }
-  if (host === 'scusmile-manage-test.depotnextdoor.com') {
-    return code.ZsyTest;
-  }
-  if (host === 'localhost:8000') {
-    return code.PosTest;
-  }
-  return 'null';
+export function getCode(host: string) {
+  const config = require('./config.json');
+  const code = config[host];
+  return code;
 }
