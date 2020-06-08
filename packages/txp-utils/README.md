e43d4f9a8a39f93cf487e480c8eb0e241e349274
DaoYuan <txp1035@gmail.com>
2020-06-09 01:29:32 +0800

    feat: 表格通用

---
+++ packages/txp-utils/README.md
@@ -0,0 +1,13 @@
+# 常用工具类
+
+## antd
+
+antd 常用组件属性提取
+
+### table
+
+一个表格至少应该有 columns（至少有 title、dataIndex、width）、dataSource
+
+#### 常用的
+
+自动计算滚动值、有边框、自动 key 值（优先 id，其次整条数据 json 化）
--- packages/txp-utils/src/antd/table.ts
+++ packages/txp-utils/src/antd/table.ts
@@ -1,17 +1,20 @@
-export const common = {
-  scroll: {
-    x: true,
-  },
-  bordered: true,
-  rowKey: (record: any, index: any) => {
-    const obj = {
-      index,
-      ...record,
-    };
-    const { id } = record;
-    if (id) {
-      return id;
-    }
-    return JSON.stringify(obj);
-  },
+export const common = (columns, dataSource) => {
+  const width = 100;
+  return {
+    scroll: {
+      x: true,
+    },
+    bordered: true,
+    rowKey: (record: any, index: any) => {
+      const obj = {
+        index, // 防止后端返回一样的数据
+        ...record,
+      };
+      const { id } = record;
+      if (id) {
+        return id;
+      }
+      return JSON.stringify(obj);
+    },
+  };
 };
