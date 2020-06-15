3a77f1c8cbd4b84417e6b6b2c77af25e6d0e50cf
DaoYuan <txp1035@gmail.com>
2020-06-15 11:57:49 +0800

    feat: 库改造

--- packages/txp-utils/src/index.ts
+++ packages/txp-utils/src/index.ts
@@ -1,6 +1,2 @@
-import * as object from './object';
-import * as jsCalculation from './jsCalculation';
-import * as other from './other';
-
-const utils = { ...object, ...jsCalculation, ...other };
-export default utils;
+export * from './object';
+export * from './other';
