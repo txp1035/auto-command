46a5c8ebfb9d553ef2831af5681d8935114b8371
DaoYuan <txp1035@gmail.com>
2020-06-16 14:24:06 +0800

    feat: 导出修改

--- packages/crons/translate/index.js
+++ packages/crons/translate/index.js
@@ -1,32 +1,3 @@
-"use strict";
+const mobile = require('cronstrue/i18n');
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-function _react() {
-  const data = _interopRequireDefault(require("react"));
-
-  _react = function _react() {
-    return data;
-  };
-
-  return data;
-}
-
-function _cronstrue() {
-  const data = _interopRequireDefault(require("cronstrue"));
-
-  _cronstrue = function _cronstrue() {
-    return data;
-  };
-
-  return data;
-}
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var _default = _cronstrue().default;
-
-exports.default = _default;
+module.exports = mobile;
