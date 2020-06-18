04486fa30298b55393bd0bba4a356c08ece2ee48
DaoYuan <txp1035@gmail.com>
2020-06-19 02:35:45 +0800

    feat: 测试集合

--- packages/txp-first/src/index.ts
+++ packages/txp-first/src/index.ts
@@ -1,10 +1,8 @@
 import moment from 'txp/util/moment';
+import { join } from 'txp/util';
 import { date } from 'txp/util/constants';
-import { join } from 'txp-utils';
-// import moment from 'moment';
+// import { join } from 'txp-utils';
 
-// export { moment };
-// export { utils };
 export function main() {
   console.log('moment测试', moment().format(date));
   console.log('join测试', join([1, 2, 3]));
