9d457d4a4fc23f59f4cb0e865cacf4d8d4f1cf1c
DaoYuan <txp1035@gmail.com>
2020-06-19 02:34:37 +0800

    feat: 测试工具类

---
+++ packages/txp-first/src/index.test.ts
@@ -0,0 +1,5 @@
+import { main } from './index';
+
+test('checkString', () => {
+  expect(main());
+});
--- packages/txp-first/src/index.ts
+++ packages/txp-first/src/index.ts
@@ -1,1 +1,11 @@
-console.log('This is my first NPM package, which was used for testing. --txp');
+import moment from 'txp/util/moment';
+import { date } from 'txp/util/constants';
+import { join } from 'txp-utils';
+// import moment from 'moment';
+
+// export { moment };
+// export { utils };
+export function main() {
+  console.log('moment测试', moment().format(date));
+  console.log('join测试', join([1, 2, 3]));
+}
