7fba5dba791d2fe6aede3383a638dff53f061673
DaoYuan <txp1035@gmail.com>
2020-07-11 00:51:50 +0800

    feat: 文件生成

--- packages/txp-first/src/gitDiff.ts
+++ packages/txp-first/src/gitDiff.ts
@@ -1,35 +1,14 @@
-const child = require('child_process');
-const shell = require('shelljs'); // 执行文件操作
+import { outputFileSync } from 'fs-extra';
+import { join } from 'path';
 
-const { spawn } = child;
+const child = require('child_process');
 
 export default () => {
-  // console.log('123');
-  shell.exec('git symbolic-ref --short -q HEAD', (err, sto) => {
-    // console.log('123', sto);
+  child.exec('git symbolic-ref --short -q HEAD', (err, sto) => {
     const str = `git diff master ${sto.replace(/\n/g, '')} --stat-width=2000 --stat-graph-width=1`;
-    console.log('zf', str);
-    shell.exec(str, (errs, stos) => {
-      console.log('222', stos);
+    child.exec(str, (errs, stos) => {
+      const paths = join(process.cwd(), '/auto/diff.txt');
+      outputFileSync(paths, stos, 'utf-8');
     });
   });
-
-  // const a = shell.exec('git log');
-  // console.log(a);
-  // child.exec('git symbolic-ref --short -q HEAD', (err, sto) => {
-  //   const diffStr = `git diff master ${sto}`;
-  //   const diff = spawn(diffStr, ['-spa-stat-width=2000', '--stat-graph-width=1']);
-  //   // 捕获标准输出并将其打印到控制台
-  //   diff.stdout.on('data', data => {
-  //     console.log('standard output:\n' + data);
-  //   });
-  //   // 捕获标准错误输出并将其打印到控制台
-  //   diff.stderr.on('data', data => {
-  //     console.log('standard error output:\n' + data);
-  //   });
-  //   // 注册子进程关闭事件
-  //   diff.on('exit', (code, signal) => {
-  //     console.log('child process eixt ,exit:' + code);
-  //   });
-  // });
 };
