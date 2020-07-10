d870a21a10d30ed39452accb9a0e26bd668f2ffe
DaoYuan <txp1035@gmail.com>
2020-07-11 00:58:42 +0800

    feat: 完成对比

--- packages/txp-first/src/gitDiff.ts
+++ packages/txp-first/src/gitDiff.ts
@@ -4,11 +4,9 @@ import { join } from 'path';
 const child = require('child_process');
 
 export default () => {
-  child.exec('git symbolic-ref --short -q HEAD', (err, sto) => {
-    const str = `git diff master ${sto.replace(/\n/g, '')} --stat-width=2000 --stat-graph-width=1`;
-    child.exec(str, (errs, stos) => {
-      const paths = join(process.cwd(), '/auto/diff.txt');
-      outputFileSync(paths, stos, 'utf-8');
-    });
+  const str = 'git diff master --stat-width=2000 --stat-graph-width=1';
+  child.exec(str, (err, sto) => {
+    const paths = join(process.cwd(), '/auto/diff.txt');
+    outputFileSync(paths, sto, 'utf-8');
   });
 };
