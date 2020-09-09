437822769e5e0a560b52f2d411e6db9995acc061
DaoYuan <txp1035@gmail.com>
2020-09-09 13:29:44 +0800

    feat: autocmd

---
+++ packages/auto-cmd/src/cli.ts
@@ -0,0 +1,21 @@
+import gitDiff from './gitDiff';
+
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: '请选择需要执行的任务',
+      name: 'auto',
+      default: '分支自动对比',
+      prefix: '****',
+      suffix: ' ****',
+      choices: ['分支自动对比', '待开发', '待开发'],
+    },
+  ])
+  .then(answer => {
+    if (answer.auto === '分支自动对比') {
+      gitDiff();
+    }
+  });
