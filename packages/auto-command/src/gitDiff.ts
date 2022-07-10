61628f0a4ec0ba7e3fb4f6e48154fc8a477e77a3
DaoYuan <txp1035@gmail.com>
2022-07-11 00:03:58 +0800

    refactor: optimize translate

--- packages/auto-command/src/gitDiff.ts
+++ packages/auto-command/src/gitDiff.ts
@@ -5,8 +5,7 @@ const child = require('child_process');
 
 export default () => {
   const str = 'git diff master --stat-width=2000 --stat-graph-width=1';
-  // @ts-ignore
-  child.exec(str, (err, sto) => {
+  child.exec(str, (_: any, sto: any) => {
     const paths = join(process.cwd(), '/autoFile/diffLog.txt');
     outputFileSync(paths, sto, 'utf-8');
   });
