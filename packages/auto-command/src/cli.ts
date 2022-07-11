846c60a9bcb193da2ed9ea7da73170b1a2f022e5
DaoYuan <txp1035@gmail.com>
2022-07-12 00:07:40 +0800

    feat: add cmd type (#13)

--- packages/auto-command/src/cli.ts
+++ packages/auto-command/src/cli.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 import { existsSync } from 'fs';
+import yParser from 'yargs-parser';
 import gitDiff from './gitDiff';
 import translate from './translate';
 import * as readEsmAndCjs from './readEsmAndCjs';
@@ -21,6 +22,7 @@ function getParams(): ConfigType {
   }
   return config;
 }
+
 function checkVersion() {
   const nodeVersion = process.versions.node;
   const versions = nodeVersion.split('.');
@@ -33,25 +35,45 @@ function checkVersion() {
     process.exit(1);
   }
 }
-checkVersion();
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: '请选择需要执行的任务',
-      name: 'auto',
-      default: '分支自动对比',
-      prefix: '****',
-      suffix: ' ****',
-      choices: ['分支自动对比', '自动翻译'],
-    },
-  ])
 
-  .then((answer: any) => {
-    if (answer.auto === '分支自动对比') {
-      gitDiff();
-    }
-    if (answer.auto === '自动翻译') {
-      translate(getParams().translate);
-    }
-  });
+checkVersion();
+
+const args = yParser(process.argv.slice(2), {
+  alias: {
+    version: ['v'],
+    help: ['h'],
+    type: ['t'],
+  },
+  boolean: ['version'],
+});
+
+if (args.version && !args._[0]) {
+  args._[0] = 'version';
+  const { name, version } = require('../package.json');
+  console.log(`${name}@${version}`);
+} else if (args.type && !args._[0]) {
+  if (args.type === 'translate') {
+    translate(getParams().translate);
+  }
+} else {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: '请选择需要执行的任务',
+        name: 'auto',
+        default: '分支自动对比',
+        prefix: '****',
+        suffix: ' ****',
+        choices: ['分支自动对比', '自动翻译'],
+      },
+    ])
+    .then((answer: any) => {
+      if (answer.auto === '分支自动对比') {
+        gitDiff();
+      }
+      if (answer.auto === '自动翻译') {
+        translate(getParams().translate);
+      }
+    });
+}
