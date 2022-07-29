9bb21739423015ee3e663e0a3e5e205613cb0a7f
DaoYuan <txp1035@gmail.com>
2022-07-29 19:13:07 +0800

    feat: utils-node init

---
+++ packages/utils-node/src/index.ts
@@ -0,0 +1,15 @@
+import chalk from '../compiled/chalk';
+import fsExtra from '../compiled/fs-extra';
+import lodash from '../compiled/lodash';
+import yParser from 'yargs-parser';
+import * as logger from './logger';
+
+export {
+  /** only node */
+  chalk,
+  fsExtra,
+  logger,
+  yParser,
+  /** all */
+  lodash,
+};
