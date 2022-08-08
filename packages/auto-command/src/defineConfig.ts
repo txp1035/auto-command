5dc7764c3eab93c516ea364381631207b86180a1
DaoYuan <txp1035@gmail.com>
2022-08-09 02:09:53 +0800

    chore: type

--- packages/auto-command/src/defineConfig.ts
+++ packages/auto-command/src/defineConfig.ts
@@ -1,7 +1,7 @@
 import type { TranslateConfig } from './translate';
 import type { FastElectronConfig } from './fastElectron';
 
-export type ConfigType = { translate: TranslateConfig; fastElectron: FastElectronConfig };
+export type ConfigType = { translate?: TranslateConfig; fastElectron?: FastElectronConfig };
 
 export function defineConfig(config: ConfigType): ConfigType {
   return config;
