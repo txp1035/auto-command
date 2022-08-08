073cd7bcce9979e27a84ef213ef4ac4987c56ee8
DaoYuan <txp1035@gmail.com>
2022-08-09 00:16:20 +0800

    feat: add pkg for node-utils

--- packages/utils-node/src/index.ts
+++ packages/utils-node/src/index.ts
@@ -1,15 +1,23 @@
 import chalk from '../compiled/chalk';
 import fsExtra from '../compiled/fs-extra';
 import lodash from '../compiled/lodash';
+import crossSpawn from '../compiled/cross-spawn';
+import inquirer from '../compiled/inquirer';
+import Mustache from '../compiled/mustache';
+import axios from '../compiled/axios';
 import yParser from 'yargs-parser';
 import * as logger from './logger';
 
 export {
   /** only node */
+  crossSpawn,
+  Mustache,
   chalk,
+  inquirer,
   fsExtra,
   logger,
   yParser,
   /** all */
+  axios,
   lodash,
 };
