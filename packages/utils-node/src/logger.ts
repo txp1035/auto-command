2a4355c62a567dda249ca87056f5c39e879ff1b3
DaoYuan <txp1035@gmail.com>
2022-08-10 18:07:20 +0800

    fix: import pkg

--- packages/utils-node/src/logger.ts
+++ packages/utils-node/src/logger.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import pino, { type Logger } from 'pino';
-import chalk from 'chalk';
-import fsExtra from 'fs-extra';
+import chalk from '../compiled/chalk';
+import fsExtra from '../compiled/fs-extra';
 
 const loggerDir = join(process.cwd(), 'node_modules/.cache/logger');
 const loggerPath = join(loggerDir, 'txpjs.log');
