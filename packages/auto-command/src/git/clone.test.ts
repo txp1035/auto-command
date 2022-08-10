f53149f01090fcd1ed6b800a49b6faf90eb57886
DaoYuan <txp1035@gmail.com>
2022-08-10 19:54:19 +0800

    fix: clone

--- packages/auto-command/src/git/clone.test.ts
+++ packages/auto-command/src/git/clone.test.ts
@@ -1,10 +1,10 @@
 import { urlTransition, getSourceUrl } from './clone';
 
 test('urlTransition', () => {
-  const https = 'https://github.com/txp1035/umi-plugin-atpl.git';
-  const ssh = 'git@github.com:txp1035/umi-plugin-atpl.git';
-  const url = 'https://github.com/txp1035/umi-plugin-atpl';
-  const name = 'umi-plugin-atpl';
+  const https = 'https://github.com/txp1035/umi.git';
+  const ssh = 'git@github.com:txp1035/umi.git';
+  const url = 'https://github.com/txp1035/umi';
+  const name = 'umi';
   expect(urlTransition(https)).toEqual({ url, name });
   expect(urlTransition(ssh)).toEqual({ url, name });
   expect(() => {
@@ -12,8 +12,8 @@ test('urlTransition', () => {
   }).toThrow('请检查你的url');
 });
 test('getSourceUrl', async () => {
-  const url = 'https://github.com/txp1035/umi-plugin-atpl';
-  const sourceUrl = 'https://github.com/';
+  const url = 'https://github.com/txp1035/umi';
+  const sourceUrl = 'https://github.com/umijs/umi';
   const res = await getSourceUrl(url);
   expect(res).toBe(sourceUrl);
 });
--- packages/auto-command/src/git/clone.ts
+++ packages/auto-command/src/git/clone.ts
@@ -1,21 +1,22 @@
 import { crossSpawn } from '@txpjs/utils-node';
-import { axios, logger } from '@txpjs/utils-node';
+import { axios, logger, fetch } from '@txpjs/utils-node';
 import * as cheerio from 'cheerio';
+import { join } from 'path';
 
-export function urlTransition(url: string): { newUrl: string; name: string } {
+export function urlTransition(preUrl: string): { url: string; name: string } {
   const httpsReg = /^(?<str>https.*\/(?<name>[^\/]*))\.git$/;
   const sshReg = /^git\@github\.com\:(?<str>.*\/(?<name>[^\/]*))\.git$/;
   let obj;
-  if (httpsReg.test(url)) {
+  if (httpsReg.test(preUrl)) {
     obj = {
-      newUrl: `${httpsReg.exec(url)?.groups?.str}`,
-      name: `${httpsReg.exec(url)?.groups?.name}`,
+      url: `${httpsReg.exec(preUrl)?.groups?.str}`,
+      name: `${httpsReg.exec(preUrl)?.groups?.name}`,
     };
   }
-  if (sshReg.test(url)) {
+  if (sshReg.test(preUrl)) {
     obj = {
-      newUrl: `https://github.com/${sshReg.exec(url)?.groups?.str}`,
-      name: `${sshReg.exec(url)?.groups?.name}`,
+      url: `https://github.com/${sshReg.exec(preUrl)?.groups?.str}`,
+      name: `${sshReg.exec(preUrl)?.groups?.name}`,
     };
   }
   if (obj === undefined) {
@@ -27,9 +28,32 @@ export function urlTransition(url: string): { newUrl: string; name: string } {
 export async function getSourceUrl(url: string) {
   let res;
   try {
+    fetch(url, {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'max-age=0',
+        'if-none-match': 'W/"d042cbe5c0406cce0d0094c4db7009ee"',
+        'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+      referrerPolicy: 'no-referrer-when-downgrade',
+      body: null,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+    });
     res = await axios.get(url);
   } catch (error) {
-    throw new Error('网络请求失败');
+    logger.error('网络请求失败');
+    throw error;
   }
   const html = res.data;
   const $ = cheerio.load(html);
@@ -38,19 +62,18 @@ export async function getSourceUrl(url: string) {
 }
 
 export default async (url: string) => {
-  const { newUrl, name } = urlTransition(url);
   logger.wait(`git clone ${url}`);
   crossSpawn.sync('git', ['clone', url], {
     stdio: 'inherit',
   });
-  logger.event(`cd ${name}`);
-  crossSpawn.sync('cd', [name], {
-    stdio: 'inherit',
-  });
+  const { url: newUrl, name } = urlTransition(url);
+  const clonePath = join(process.cwd(), name);
+  logger.info(`your respository: ${clonePath}`);
   logger.wait('getSourceUrl');
   const sourceUrl = await getSourceUrl(newUrl);
   logger.event(`git remote add source ${sourceUrl}`);
   crossSpawn.sync('git', ['remote', 'add', 'source', sourceUrl], {
     stdio: 'inherit',
+    cwd: clonePath,
   });
 };
